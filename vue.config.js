392e32d5e148a4bd2c6c5ff485e1051aca30ac53
sunshineluo <37474599+sunshineluo@users.noreply.github.com>
2023-04-28 17:43:45 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
 	devServer: {
 		proxy: {
 			'/api': {
-				target: '', // Vercel/腾讯云部署后的域名
+				target: 'https://lianghj.top:3000/', // Vercel/腾讯云部署后的域名
 				ws: true,
 				pathRewrite: {
 					'^/api': '', //路径重写，将接口路径中以/api开头的部分替换掉
